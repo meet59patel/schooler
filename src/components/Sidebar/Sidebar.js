7e8949fc5bda5e1e0a1d996a67677fba24bc7ec2
Manan Kevadiya <43081866+manan2501@users.noreply.github.com>
2020-10-02 21:13:06 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -41,21 +41,23 @@ function Sidebar(props) {
   };
   // creates the links that appear in the left menu / Sidebar
   const createLinks = (routes)=>{
-    return routes.map((prop, key) => {
-      return (
-        <NavItem key={key}>
-          <NavLink
-            to={prop.layout + prop.path}
-            tag={NavLinkRRD}
-            onClick={closeCollapse}
-            activeClassName="active"
-          >
-            <i className={prop.icon} />
-            {prop.name}
-          </NavLink>
-        </NavItem>
-      );
-    });
+      return routes.map((prop, key) => {
+        if(prop.layout=="/admin"){
+          return (
+            <NavItem key={key}>
+              <NavLink
+                to={prop.layout + prop.path}
+                tag={NavLinkRRD}
+                onClick={closeCollapse}
+                activeClassName="active"
+              >
+                <i className={prop.icon} />
+                {prop.name}
+              </NavLink>
+            </NavItem>
+          );
+        }
+      });
   };
 
   
