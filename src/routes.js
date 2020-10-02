2a2ef2ae5cc82bc142d299ec8f2ac4ddb81b3c0c
Manan Kevadiya <43081866+manan2501@users.noreply.github.com>
2020-10-02 21:08:58 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -69,11 +69,15 @@ var routes = [
   },
   {
     path: "/login",
+    name: "Login",
+    icon: "ni ni-key-25 text-info",
     component: Login,
     layout: "/auth"
   },
   {
     path: "/register",
+    name: "Register",
+    icon: "ni ni-circle-08 text-pink",
     component: Register,
     layout: "/auth"
   }
