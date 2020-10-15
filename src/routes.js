6c6158569177b93b2f9c4103aeeef0638a79fb18
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-10-15 23:53:29 +0530

    commit on removal of Register buttons and fiunctionalities

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import Index from "./views/Index.js";
 import Profile from "./views/examples/Profile.js";
 import Maps from "./views/examples/Maps.js";
-import Register from "./views/examples/Register.js";
 import Login from "./views/examples/Login.js";
 import Tables from "./views/examples/Tables.js";
 import Icons from "./views/examples/Icons.js";
@@ -47,13 +46,6 @@ var routes = [
     name: "Login",
     icon: "ni ni-key-25 text-info",
     component: Login,
-    layout: "/auth"
-  },
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    component: Register,
     layout: "/auth"
   }
 ];
